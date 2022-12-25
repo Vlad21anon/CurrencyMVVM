5b9a0a2e486b3f6876763d51dbb2042752148cfe
Vlad Legkiy <ukadoV@gmail.com>
2022-12-25 18:11:31 +0200

    all dependencies have implemented

---
+++ app/src/main/java/com/ukadovlad21/mvvm1/CurrencyApplication.kt
@@ -0,0 +1,6 @@
+package com.ukadovlad21.mvvm1
+
+import android.app.Application
+
+class CurrencyApplication: Application() {
+}
---
+++ app/src/main/java/com/ukadovlad21/mvvm1/utils/Constant.kt
@@ -0,0 +1,9 @@
+package com.ukadovlad21.mvvm1.utils
+
+class Constant {
+    companion object {
+        const val BASE_URL = "https://cdn.jsdelivr.net"
+
+    }
+}
+//https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd/uah.json
